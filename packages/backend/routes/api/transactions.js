03c92f06c42142315b8e965dabcd3e7943431f7c
Krzysztof Olejniczak <krzysztof.olejniczak@gmail.com>
2023-07-16 22:07:02 +0200

    add swagger-ui on / route

--- packages/backend/routes/api/transactions.js
+++ packages/backend/routes/api/transactions.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   - name: Transactions
+ *     description: Endpoints related to transactions
+ */
+
 router.get("/", async (req, res, next) => {
   res.json({ message: "template test message" });
 });
