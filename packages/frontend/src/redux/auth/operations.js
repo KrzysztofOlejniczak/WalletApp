3b47f6faadddd6e5a7bcd9d5b0da89ad943e1c0b
Marlena Dulęba <marlena_duleba@o2.pl>
2023-07-18 18:56:49 +0200

    path for developer server

--- packages/frontend/src/redux/auth/operations.js
+++ packages/frontend/src/redux/auth/operations.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
+// axios.defaults.baseURL = 'http://localhost:3000/api';
 
 // Utility to add JWT
 const setAuthHeader = (token) => {
