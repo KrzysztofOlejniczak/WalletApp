ea0602be232de6542cb1e7adbe1bac54c0ef2da2
Marlena Dulęba <marlena_duleba@o2.pl>
2023-07-18 18:23:51 +0200

    finance reducer added

---
+++ packages/frontend/src/redux/finance/operations.js
@@ -0,0 +1,88 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+axios.defaults.baseURL = process.env.REACT_APP_BACKEND_URL;
+
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+export const fetchTransactions = createAsyncThunk(
+  'finance/fetchTransactions',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue('Token is missing');
+    }
+
+    try {
+      setAuthHeader(persistedToken);
+      const res = await axios.get('/finance/transactions');
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const fetchBalance = createAsyncThunk(
+  'finance/fetchBalance',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue('Token is missing');
+    }
+
+    try {
+      setAuthHeader(persistedToken);
+      const res = await axios.get('/finance/balance');
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addTransaction = createAsyncThunk(
+  'finance/addTransaction',
+  async (transactionData, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue('Token is missing');
+    }
+
+    try {
+      setAuthHeader(persistedToken);
+      const res = await axios.post('/finance/transactions', transactionData);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteTransaction = createAsyncThunk(
+  'finance/deleteTransaction',
+  async (transactionId, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue('Token is missing');
+    }
+
+    try {
+      setAuthHeader(persistedToken);
+      await axios.delete(`/finance/transactions/${transactionId}`);
+      return transactionId;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
---
+++ packages/frontend/src/redux/finance/slice.js
@@ -0,0 +1,63 @@
+import { createSlice } from '@reduxjs/toolkit';
+import {logOut } from '../auth/operations';
+import { fetchTransactions, fetchBalance, addTransaction, deleteTransaction } from './operations.js';
+
+const handlePending = (state) => {
+  state.loading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.loading = false;
+  state.error = action.payload;
+};
+
+const financeSlice = createSlice({
+  name: 'finance',
+  initialState: {
+    transactions: [],
+    balance: null,
+    loading: false,
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchTransactions.pending, handlePending)
+      .addCase(fetchTransactions.rejected, handleRejected)
+      .addCase(fetchTransactions.fulfilled, (state, action) => {
+        state.transactions = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(fetchBalance.pending, handlePending)
+      .addCase(fetchBalance.rejected, handleRejected)
+      .addCase(fetchBalance.fulfilled, (state, action) => {
+        state.balance = action.payload;
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(addTransaction.pending, handlePending)
+      .addCase(addTransaction.rejected, handleRejected)
+      .addCase(addTransaction.fulfilled, (state, action) => {
+        state.transactions.push(action.payload);
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(deleteTransaction.pending, handlePending)
+      .addCase(deleteTransaction.rejected, handleRejected)
+      .addCase(deleteTransaction.fulfilled, (state, action) => {
+        state.transactions = state.transactions.filter(
+          (transaction) => transaction.id !== action.payload.id
+        );
+        state.loading = false;
+        state.error = null;
+      })
+      .addCase(logOut.fulfilled, (state) => {
+        state.transactions = [];
+        state.balance = null;
+        state.loading = false;
+        state.error = null;
+      });
+  },
+});
+
+export const financeReducer = financeSlice.reducer;
