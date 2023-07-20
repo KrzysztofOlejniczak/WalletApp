187722087a4c5d0babee17ff777f573509fd725f
Marlena Dulęba <marlena_duleba@o2.pl>
2023-07-20 18:17:10 +0200

    unnecessary selector removed

--- packages/frontend/src/redux/finance/selectors.js
+++ packages/frontend/src/redux/finance/selectors.js
@@ -2,6 +2,4 @@ export const selectTransactions = (state) => state.finance.transactions;
 
 export const selectBalance = (state) => state.finance.balance;
 
-export const selectLoading = (state) => state.finance.loading;
-
 export const selectError = (state) => state.finance.error;
