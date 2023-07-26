50a02d0f340459e72985f8b4dc3018582794e348
Marlena Dulęba <marlena_duleba@o2.pl>
2023-07-26 11:27:02 +0200

    react-media removed

--- packages/frontend/src/components/navigation/navigation.jsx
+++ packages/frontend/src/components/navigation/navigation.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Media from 'react-media';
-import { Box, Link } from '@mui/material';
+import { Box, Link, useMediaQuery } from '@mui/material';
 
 export const Navigation = () => {
+  const isMobile = useMediaQuery((theme) => theme.breakpoints.down('md'));
+
   return (
-    <Box component="nav" sx={{display: 'flex', gap: 2, flexWrap: 'wrap'}}>
+    <Box component="nav" sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
       <Link variant="nav" to="/home" component={NavLink}>
         Home
       </Link>
       <Link variant="nav" to="/diagram" component={NavLink}>
         Statistics
       </Link>
-      <Media query="(max-width: 767px)">
-        {(matches) =>
-          matches && (
-            <Link variant="nav" to="/currency" component={NavLink}>
-              Currency
-            </Link>
-          )
-        }
-      </Media>
+      {isMobile && (
+        <Link variant="nav" to="/currency" component={NavLink}>
+          Currency
+        </Link>
+      )}
     </Box>
   );
 };
