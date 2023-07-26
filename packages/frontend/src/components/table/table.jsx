232748106165ba9b24c02974463a463bcc693e6d
Marlena Dulęba <marlena_duleba@o2.pl>
2023-07-26 18:32:18 +0200

    buttons added

--- packages/frontend/src/components/table/table.jsx
+++ packages/frontend/src/components/table/table.jsx
@@ -7,12 +7,32 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Button,
+  IconButton,
+  SvgIcon,
 } from '@mui/material';
 
-export default function TransactionsTable({ data }) {
+import { ReactComponent as Edit } from '../../images/edit.svg';
+import {StyledTableCell} from './table.styles'
+
+export default function TransactionsTable({
+  data,
+  handleEditTransaction,
+  handleDeleteTransaction,
+}) {
   const formatDate = (date) => {
     const options = { year: '2-digit', month: '2-digit', day: '2-digit' };
     return new Date(date).toLocaleDateString('pl-PL', options);
+  };
+
+  const formatAmount = (amount) => {
+    const formattedAmount = parseFloat(amount).toLocaleString('pl-PL', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+      useGrouping: true,
+    });
+
+    return formattedAmount.replace(',', '.');
   };
 
   return (
@@ -20,26 +40,46 @@ export default function TransactionsTable({ data }) {
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell align="left">Date</TableCell>
-            <TableCell align="left">Type</TableCell>
-            <TableCell align="left">Category</TableCell>
-            <TableCell align="left">Comment</TableCell>
-            <TableCell align="right">Sum</TableCell>
+            <StyledTableCell align="left">Date</StyledTableCell>
+            <StyledTableCell align="left">Type</StyledTableCell>
+            <StyledTableCell align="left">Category</StyledTableCell>
+            <StyledTableCell align="left">Comment</StyledTableCell>
+            <StyledTableCell align="right">Sum</StyledTableCell>
+            <StyledTableCell align="right"></StyledTableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((item) => (
+          {data.map((el) => (
             <TableRow
-              key={item._id}
+              key={el._id}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell align="left">{formatDate(item.date)}</TableCell>
-              <TableCell align="left">
-                {item.isExpense === true ? '-' : '+'}
-              </TableCell>
-              <TableCell align="left">{item.category}</TableCell>
-              <TableCell align="left">{item.comment}</TableCell>
-              <TableCell align="right">{item.amount}</TableCell>
+              <StyledTableCell align="left">{formatDate(el.date)}</StyledTableCell>
+              <StyledTableCell align="left">
+                {el.isExpense === true ? '-' : '+'}
+              </StyledTableCell>
+              <StyledTableCell align="left">{el.category}</StyledTableCell>
+              <StyledTableCell align="left">{el.comment}</StyledTableCell>
+              <StyledTableCell align="right">{formatAmount(el.amount)}</StyledTableCell>
+              <StyledTableCell align="center">
+                <IconButton
+                  aria-label="edit"
+                  onClick={() => handleEditTransaction(el)}
+                >
+                  <SvgIcon
+                    component={Edit}
+                    viewBox="0 0 14 14"
+                    sx={{ fill: 'none' }}
+                  />
+                </IconButton>
+                <Button
+                  variant="delete"
+                  type="button"
+                  onClick={() => handleDeleteTransaction(el._id)}
+                >
+                  Delete
+                </Button>
+              </StyledTableCell>
             </TableRow>
           ))}
         </TableBody>
