a86f08e8165f550d9fa6f058aff712ae20d4f541
Marlena Dulęba <marlena_duleba@o2.pl>
2023-07-30 15:19:48 +0200

    styles corrections

--- packages/frontend/src/pages/dashboard/dashboard.styles.js
+++ packages/frontend/src/pages/dashboard/dashboard.styles.js
@@ -1,14 +1,14 @@
-import { Box, Container } from '@mui/material';
+import { Box, Container, Grid } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import ellipsePurple from '../../images/ellipse-purple.svg';
 import ellipsePink from '../../images/ellipse-pink.svg';
-import '../../stylesheet/vars.css';
 
 export const DashboardWrapper = styled('div')({
   width: '100%',
   height: '100%',
   display: 'flex',
   flexDirection: 'column',
+
 
   '@media (max-width: 767px)': {
     '& .MuiContainer-fixed': {
@@ -22,15 +22,15 @@ export const DashboardWrapper = styled('div')({
   },
   '@media (min-width: 1280px)': {
     '& .MuiContainer-fixed': {
-      padding: '0 16px 0 16px',
+      padding: '0 16px',
     },
   },
 });
 
 export const ContainerWrapper = styled('div')({
   position: 'relative',
-  height: '100%',
-  width: '100%',
+  height: '100vh',
+  width: '100vw',
   display: 'flex',
   flexDirection: 'row',
   alignItems: 'center',
@@ -42,7 +42,7 @@ export const ContainerWrapper = styled('div')({
     width: '100%',
     height: '100%',
     zIndex: -1,
-    backgroundColor: 'var(--gray-background)',
+    backgroundColor: 'var(--secondary-background-color)',
     opacity: 1,
     '@media (min-width: 768px)': {
       background: `url(${ellipsePurple}), url(${ellipsePink}) `,
@@ -61,8 +61,22 @@ export const StyledContainer = styled(Container)({
 export const Vector = styled(Box)({
   width: 0,
   height: '100%',
-  borderLeft: '1px solid var(--gray-divider-vertical)',
+  borderLeft: '1px solid var(--transactions-line)',
   boxShadow:
     'rgba(0, 0, 0, 0.05) -1px 0px 0px, rgba(255, 255, 255, 0.6) 1px 0px 0px',
   zIndex: 1,
 });
+
+export const NavGrid = styled(Grid)({
+  display: 'flex',
+  flexDirection: 'column',
+  '@media (min-width: 768px) and (max-width: 1279px)': {
+    display: 'row',
+    flexWrap: 'wrap',
+  },
+});
+
+export const DataGrid = styled(Grid)({
+  display: 'flex',
+});
+
