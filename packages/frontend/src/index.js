81c61e34d97bf1b74cb7693a6db53de4a8175a7f
Marlena Dulęba <marlena_duleba@o2.pl>
2023-07-30 16:00:22 +0200

    name corrected

--- packages/frontend/src/index.js
+++ packages/frontend/src/index.js
@@ -7,7 +7,7 @@ import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { HelmetProvider } from 'react-helmet-async';
 import { ThemeProvider } from '@mui/material/styles';
-import { theme } from './stylesheet/utils/breakpoinsMUI';
+import { theme } from './stylesheet/utils/breakpointsMUI';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
