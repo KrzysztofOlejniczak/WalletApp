48bc3576a39ec482c1cc8c2354538ef7256e3d97
Marcin Wrembel <marcin.wrembel@interia.pl>
2023-08-01 22:01:25 +0200

    update

--- packages/frontend/src/components/textInput/textInputConfirm.js
+++ packages/frontend/src/components/textInput/textInputConfirm.js
@@ -6,15 +6,20 @@ const TextInputConfirm = ({ label, ...props }) => {
   const [field] = useField(props);
   return (
     <>
-      <ErrorMessage
-        component="div"
-        name={field.name}
-        style={{ color: '#FF6596' }}
-      />
       <label htmlFor={field.name} className="label2">
         {label}
         <input {...field} {...props} autoComplete="off" />
       </label>
+      <ErrorMessage
+        component="div"
+        name={field.name}
+        style={{
+          color: '#FF6596',
+          fontSize: '10px',
+          margin: '10px 0 10px 0',
+          alignSelf: 'start',
+        }}
+      />
     </>
   );
 };
