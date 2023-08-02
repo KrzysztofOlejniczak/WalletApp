4c1e6ee73b4ebaf612528b0fcc3694327f3d4d28
Marlena Dulęba <marlena_duleba@o2.pl>
2023-08-02 21:20:24 +0200

    another files removed

--- packages/frontend/src/routing.js
+++ packages/frontend/src/routing.js
@@ -56,7 +56,13 @@ const Routing = () => {
       <Route path="/currency" element={<DashboardPage />}>
         <Route
           index
-          element={isMobile ?  <PrivateRoute redirectTo="/login" component={<Currency />} /> : <Navigate to="/login" />}
+          element={
+            isMobile ? (
+              <PrivateRoute redirectTo="/login" component={<Currency />} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
         />
       </Route>
 
